d09383157bd67c0fbb82037de9458ed315054e2e
Yann Chavaillez <yanncha@neree.ouranos.ca>
2019-03-15 11:48:24 -0400

    final modifications of comments

--- src/main_improved.py
+++ src/main_improved.py
@@ -1,8 +1,8 @@
 ##################################################################
-# Description: Main code to plot statistics (in histogram) of age and height of a database of men and women. Men and women are separated here to give two distinguishable distribution. Histograms are stored in ./figures.
-# Code name: ./src/main.py
+# Description: Main code to plot statistics (in histogram) of age and height of a database of men and women. Men and women are separated here to give two distinguishable distributions. Histograms are stored in ./figures.
+# Code name: ./src/main_improved.py
 # Date of creation: 2019/03/12
-# Date of last modification: 2019/03/14
+# Date of last modification: 2019/03/15
 # Contact information: Yann Chavaillaz, yann.chavaillaz@gmail.com
 ##################################################################
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
 ##################################################################
-# Description: Defintion of all the functions/modules useful to plot statistics from the database ./data/data.csv. The modules are used in ./src/main.py.
+# Description: Defintion of all the functions/modules useful to plot statistics from the database ./data/data.csv. The modules are used in ./src/main_improved.py.
 # Code name: ./src/utils.py
 # Date of creation: 2019/03/12
-# Date of last modification: 2019/03/14
+# Date of last modification: 2019/03/15
 # Contact information: Yann Chavaillaz, yann.chavaillaz@gmail.com
 ##################################################################
 
@@ -11,18 +11,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def read_data(file):
-    """ Read the content of a .csv file and putting it in the dataframe (using pandas). Headers are considered. """
+    """
+    Read the content of a .csv file and putting it in the dataframe (using pandas). Headers are considered.
+    - file is the .csv file in which the data is written
+    """
+
     data = pd.read_csv(file)
+
     return data
 
+
 def sort_data(df,gender):
-    """ Sort a dataframe of statistics of men and women. The output is a sub-dataframe containing only men or only women statistics. """
+    """
+    Sort a dataframe of statistics of men and women. The output is a sub-dataframe containing only men or only women statistics.
+    - df is the pandas dataframe containing data
+    - gender can be either 'F' or 'M'
+    """
+
     sorted_df = df[df['gender'].isin([gender])]
+
     return sorted_df
 
+
 def choose_bins(df,var,step):
-    """ Selection of bins consistent with the statistics of the dataframe. For age statitics: getting the previous integer of the minimum value as a lower limit and the next integer of the maximum value as an upper limit. For height statistics: similar, but with a 10^-1 precision. One bin per year for age, one bin every 2cm for height. """
+    """
+    Selection of bins consistent with the statistics of the dataframe. For age statitics: getting the previous integer of the minimum value as a lower limit and the next integer of the maximum value as an upper limit. For height statistics: similar, but with a 10^-1 precision. One bin per year for age, one bin every 2cm for height.
+    - df is the pandas dataframe containing data
+    - var is the variable to assess, either 'age' or 'height'
+    - step is the increment for bins intervals. Optimal option: 1. for age, 0.1 for height
+    """
+
     if step == 1.:
         bin_1   = round(np.min(df[var].values))
         bin_n   = round(np.max(df[var].values))+1
@@ -31,10 +51,21 @@ def choose_bins(df,var,step):
         bin_1   = round(np.min(df[var].values),1)
         bin_n   = round(np.max(df[var].values),1)+0.1
         bins    = int((bin_n-bin_1)*50.)
+
     return bin_1,bin_n,bins
 
+
 def plot_histogram(list,bin_1,bin_n,bins,side,label):
-    """ Plot histogram for statistics of one gender in particular. Use the left/right part of each bin if women/men statistics are plotted. """
+    """
+    Plot histogram for statistics of one gender in particular. Use the left/right part of each bin if women/men statistics are plotted.
+    - list is the np.array to asssess
+    - bin_1 is the lower limit of bins for the histogram
+    - bin_n is the upper limit of bins for the histogram
+    - bins is the interval between two bar of the histogram
+    - side defines where statistics are plotted: 'left' or 'right' of the interval
+    - label is the label appearing in the legend. E.g. 'men' or 'women'
+    """
+
     counts_list, bin_edges_list = np.histogram(list, bins=bins,range=(bin_1,bin_n))
     column_width = np.min(bin_edges_list[1:] - bin_edges_list[:-1])/3.
     if side == 'left':
@@ -42,10 +73,20 @@ def plot_histogram(list,bin_1,bin_n,bins,side,label):
     elif side == 'right':
         column_spot = 0.5*(bin_edges_list[1:]+bin_edges_list[:-1])-column_width
     plt.bar(column_spot,counts_list,label=label,alpha=0.5, width=column_width)
+
     return column_spot,column_width
 
+
 def doublehistogram(df,var,step,res,path):
-    """ Construction and saving of the figure illustrating statistics of both men and women. Figure is saved in the folder 'path'. """
+    """
+    Construction and saving of the figure illustrating statistics of both men and women. Figure is saved in the folder 'path'.
+    - df is the pandas dataframe containing data
+    - var is the variable to assess, either 'age' or 'height'
+    - step is the increment for bins intervals. Optimal option: 1. for age, 0.1 for height
+    - res is the resolution of the figure in dpi
+    - path is the path were to store the output figure
+    """
+
     df_F = sort_data(df,'F')
     df_M = sort_data(df,'M')
     bin_1,bin_n,bins = choose_bins(df,var,step)
@@ -57,4 +98,5 @@ def doublehistogram(df,var,step,res,path):
     plt.title('Distribution of '+var+' among both genders')
     plt.savefig(path+var+'_distribution.png',dpi=res)
     plt.close()
+
     return spot1,spot2,width1,width2
